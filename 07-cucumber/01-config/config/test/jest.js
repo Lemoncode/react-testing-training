d27d5016a58823337d44411985c8f12304fa6794
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-10-14 12:11:56 +0200

    implement config

--- 07-cucumber/01-config/config/test/jest.js
+++ 07-cucumber/01-config/config/test/jest.js
@@ -3,4 +3,8 @@ module.exports = {
   preset: 'ts-jest',
   restoreMocks: true,
   setupFilesAfterEnv: ['<rootDir>/config/test/setup-after.ts'],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test|steps).[jt]s?(x)',
+  ],
 };
